e84315620de2ff54879029a72c30de6ea82eb0d9
Gustavo Rodrigues <rodrigues.gustavo20@escola.pr.gov.br>
2026-08-17 08:59:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// npm init
+// npm i express
+// para executar: noe index.js
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+
+// seu código para cadastrar, ver, deletar, etc
+app.post("/produto", (req, res) => {
+    const produto = req.body
+    try {
+        const produtos = JSON.parse(fs.readFileSync("produto.json", "utf8"))
+        produtos.push(produto)
+        fs.writeFileSync("produtos.json", JSON.stringifly(produtos), "utf8")
+        res.status(201).json({mensagem: "Produto cadastrado!"})
+    } catch (error) {
+        res.status(500).json({erro: error.message})
+    }
+})
+
+app.listen(port, () => {
+    console.log("API rodando da porta " + port)
+})
